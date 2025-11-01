8a4074a4f6cc78c5f94fe000189d8ac9d86ab288
Dri Bevilaqua <dribevilaqua@gmail.com>
2025-10-31 23:04:41 -0300

    adicionando

---
+++ projetoyt/src/App.jsx
@@ -0,0 +1,16 @@
+import AddTask from "./components/AddTask";
+import Tasks from "./components/Tasks";
+
+function App() {
+  return (
+      <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+        <div className="w-[500px]">
+          <h1 className="text-3xl text-salte100 font-bold text-center">Gerenciador de Tarefas</h1>
+          <AddTask />
+          <Tasks />
+        </div> 
+      </div>
+  );
+}
+
+export default App
---
+++ projetoyt/src/components/Tasks.jsx
@@ -0,0 +1,9 @@
+function Tasks() {
+    return(
+        <div>
+            <h1>Taks</h1>
+         </div>
+    )
+}
+
+export default Tasks;

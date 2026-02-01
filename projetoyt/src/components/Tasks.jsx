dbfff716648542c30cc0a5739e5fc9b3983f597c
Dri Bevilaqua <andrielebevilaqua@gmail.com>
2026-02-01 09:45:50 -0300

    finalizando

--- projetoyt/src/components/Tasks.jsx
+++ projetoyt/src/components/Tasks.jsx
@@ -1,9 +1,43 @@
-function Tasks() {
-    return(
-        <div>
-            <h1>Taks</h1>
-         </div>
-    )
+import { Check, ChevronRight, Trash } from "lucide-react";
+import { useNavigate } from "react-router-dom";
+import Button from "./Button";
+
+function Tasks({ tasks, onTaskClick, onDeleteTaskClick }) {
+  const navigate = useNavigate();
+
+  function onSeeDetailsClick(task) {
+    const query = new URLSearchParams();
+    query.set("title", task.title);
+    query.set("description", task.description);
+
+    navigate(`/task?${query.toString()}`);
+  }
+
+  return (
+    <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
+      {tasks.map((task) => (
+        <li key={task.id} className="flex gap-2">
+          <button
+            onClick={() => onTaskClick(task.id)}
+            className={`bg-slate-400 text-left w-full flex items-center gap-2 text-white p-2 rounded-md ${
+              task.completed && "line-through"
+            }`}
+          >
+            {task.completed && <Check />}
+            {task.title}
+          </button>
+
+          <Button onClick={() => onSeeDetailsClick(task)}>
+            <ChevronRight />
+          </Button>
+
+          <Button onClick={() => onDeleteTaskClick(task.id)}>
+            <Trash />
+          </Button>
+        </li>
+      ))}
+    </ul>
+  );
 }
 
 export default Tasks;
